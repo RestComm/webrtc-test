88f1e95f693e67a71da58bba19f6f3199425f693
Antonis Tsakiridis <antonis.tsakiridis@telestax.com>
2016-06-24 13:03:06 +0000

    Fixed #63: Introduce functionality to collect and compress logs. Fixed #64: Make load-runner command line switches more user-friendly

--- load-runner.pl
+++ load-runner.pl
@@ -3,10 +3,13 @@
 use warnings;
 use strict;
 use Getopt::Long;
+#use Switch;
 
 # globals
 my $version = "0.3";
 
+# default command is to run actual load test
+my $command = "run";
 my $clientCount = "30";
 my $clientPrefix = "user";
 my $clientPassword = "1234";
@@ -22,20 +25,21 @@ my $sippOutputFile = "sipp-run.log";
 my $sippConcurrentCalls = "10";
 my $sippTotalCalls = "10000";
 my $sippCallsPerSecond = "1";
-my $cleanupBeforeRun = 1;
-my $eraseLogsOnly = 0;
-my $shutdownOnly = 0;
+my $noLogCleanup = 0;
+#my $eraseLogsOnly = 0;
+#my $shutdownOnly = 0;
 my $restcommEnableRecording = 0;
 
 # prints out a short usage for the tool
 sub printUsage
 {
 	print "load-runner.pl, Ver. $version\n";
-	#print "Usage: \$ load-runner.pl [ -s ] [-f <import file>] [-l <import log master>] [-x <export master>]\n";
-	#print "Examples:\n";
-	#print "\t\$ nreg.pl -t testcases.txt                                                - run all tests included in the test case file (full regression run)\n";
-	#print "\t\$ nreg.pl -f import.txt -l log-master.txt -x export-master.txt            - run a single import/export test\n";
-	#print "\t\$ nreg.pl -g file -f import.txt -l log-master.txt -x export-master.txt    - generate log master and export master from the given import file\n";
+	print "Usage: \$ load-runner.pl <command> [options]\n";
+	print "Examples:\n";
+	print "\t\$ load-runner.pl --command run --client-count 30 --sipp-total-calls 35000 --sipp-concurrent-calls 10 --sipp-calls-per-second 1 --client-prefix 0user --restcomm-ip 10.142.205.168 --restcomm-account-sid ACae6e420f425248d6a26948c17a9e2acf --restcomm-auth-token 3349145c827863209020dbc513c87260 --restcomm-phone-number \"+5556\"\n";
+	print "\t\$ load-runner.pl --command erase-logs\n";
+	print "\t\$ load-runner.pl --command shutdown\n";
+	print "\t\$ load-runner.pl --command collect-logs\n";
 }
 
 # kill all load test processes
@@ -120,7 +124,8 @@ if ($num_args < 0) {
 }
 
 
-my $result = GetOptions("client-count|c=s" => \$clientCount,
+my $result = GetOptions("command|c=s" => \$command,
+			"client-count=s" => \$clientCount,
 			"client-prefix=s" => \$clientPrefix,
 			"client-password=s" => \$clientPassword,
 			"client-browser=s"	=> \$clientBrowser,
@@ -133,9 +138,9 @@ my $result = GetOptions("client-count|c=s" => \$clientCount,
 			"sipp-concurrent-calls=s" => \$sippConcurrentCalls,
 			"sipp-total-calls=s" => \$sippTotalCalls,
 			"sipp-calls-per-second=s" => \$sippCallsPerSecond,
-			"cleanup-before-run=i" => \$cleanupBeforeRun,
-			"erase-logs-only=i" => \$eraseLogsOnly,
-			"shutdown-only=i" => \$shutdownOnly,
+			"no-log-cleanup" => \$noLogCleanup,
+			#"erase-logs-only" => \$eraseLogsOnly,
+			#"shutdown-only" => \$shutdownOnly,
 			"help|h" => sub { printUsage(); exit 1; });
 
 if (!$result) {
@@ -143,29 +148,56 @@ if (!$result) {
 	exit 1;
 }
 
-if ($eraseLogsOnly) {
+if ($command eq "run") {
+	# stop any prior instances of load runner
+	killProcesses();
+
+	if (!$noLogCleanup) {
+		cleanupFiles();
+	}
+
+	startXvfb();
+
+	startWebrtcTest();
+
+	if (waitForClients() != 0) {
+		print "[load-runner] Clients still unregistered; bailing out\n";
+		exit 1;
+	}
+
+	startTraffic();
+}
+elsif ($command eq "erase-logs") {
 	cleanupFiles();
 	exit 0;
 }
-
-if ($shutdownOnly) {
+elsif ($command eq "shutdown") {
 	killProcesses();
 	exit 0;
 }
+elsif ($command eq "collect-logs") {
+	my $logFiles = qx(find . -name "*log" -o -name "*log.*");
+	chomp($logFiles);
+	$logFiles =~ s/\n/ /;
 
-if ($cleanupBeforeRun) {
-	killProcesses();
-	cleanupFiles();
+	my $date = qx(date);
+	chomp($date);
+	# replace spaces in date
+	$date =~ s/ /-/g;
+	# remove colons that cause issues in the invocation
+	$date =~ s/\://g;
+
+	qx(mkdir $date);
+	
+	qx(cp $logFiles $date/);
+
+	print "Compressing logs into $date.tar.gz\n";
+	qx(tar -zcvf $date.tar.gz $date);
+	if ($? == 0) {
+		qx(rm -fr $date);
+	}
+	else {
+		print "[load-runner] Failed to compress logs";
+	}
 }
-
-startXvfb();
-
-startWebrtcTest();
-
-if (waitForClients() != 0) {
-	print "[load-runner] Clients still unregistered; bailing out\n";
-	exit 1;
-}
-
-startTraffic();
 
